bfa9cc8095a900fae713b43c13c3316b997bc5d6
fahad1998-hub <ashraffahad498@gmail.com>
2026-09-02 13:43:15 +0000

    init portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.join(__dirname, "public");
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
